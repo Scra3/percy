6c84eda3c717fb4cb93b2da3ca04618a45c5b044
alban bertolini <albanbertolini@mbp-de-alban.home>
2021-12-27 08:51:55 +0100

    test: add a lot of tests

---
+++ tests/acceptance/todomvc-2-test.js
@@ -0,0 +1,40 @@
+import { test, module } from 'qunit';
+import percySnapshot from '@percy/ember';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, fillIn, triggerKeyEvent, click } from '@ember/test-helpers';
+
+module('TodoMVC 2', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(async function () {
+    window.localStorage.clear();
+    await visit('/');
+  });
+
+  test('Loads the app 3', async function (assert) {
+    assert.dom('section.todoapp').exists();
+    await percySnapshot(assert);
+  });
+
+  test('Accepts a new todo 3', async function (assert) {
+    await fillIn('.new-todo', 'New fancy todo');
+    await triggerKeyEvent('.new-todo', 'keyup', 13);
+
+    assert.dom('.todo-list').exists({ count: 1 });
+
+    await percySnapshot('Snapshot with new todo', { widths: [300] });
+  });
+
+  test('Lets you check off a todo 3', async function (assert) {
+    await fillIn('.new-todo', 'A thing to accomplish');
+    await triggerKeyEvent('.new-todo', 'keyup', 13);
+
+    assert.dom('.todo-count').hasText('1 item left');
+
+    await click('input.toggle');
+
+    assert.dom('.todo-count').hasText('0 items left');
+
+    await percySnapshot(assert, { widths: [768, 992, 1200] });
+  });
+});
